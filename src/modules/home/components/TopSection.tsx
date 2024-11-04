23a19679d236c8bd467e66a25a62279fea9bd95e
tlleonart <tomasplleonart@gmail.com>
2024-11-04 16:51:57 -0300

    feat: added banner

--- src/modules/home/components/TopSection.tsx
+++ src/modules/home/components/TopSection.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import Image from "next/image";
+import banner from '../images/welcomeBanner.jpeg'
 
 const TopSection: FC = () => {
     return (
         <section className="mb-12">
             <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px]">
                 <Image
-                    src="/placeholder.svg?height=500&width=1920"
+                    src={banner}
                     alt="Imagen representativa de sostenibilidad y proyectos de triple impacto"
                     layout="fill"
                     objectFit="cover"
