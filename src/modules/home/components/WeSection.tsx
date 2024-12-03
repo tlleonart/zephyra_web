0ac403852dbbc6005ddd1882c64f25a25920f2d0
tlleonart <tomasplleonart@gmail.com>
2024-12-03 16:10:01 -0300

    we section modificada

--- src/modules/home/components/WeSection.tsx
+++ src/modules/home/components/WeSection.tsx
@@ -4,12 +4,15 @@ const WeSection: FC = () => {
     return (
         <section className="relative h-screen" id='about'>
             <div className="container mx-auto px-4">
-                <div className="flex flex-col w-full justify-center">
-                    <h2 className="text-4xl font-semibold text-zmain text-center mb-4">Somos consultoría en sostenibilidad</h2>
-                    <p className="text-gray-700">
-                        Convertimos el compromiso con el triple impacto en acciones concretas, diseñando e implementando estrategias que hacen de la sostenibilidad
-                        una realidad tangible para empresas y organizaciones públicas y privadas.
-                    </p>
+                <div className="flex h-screen justify-center">
+                    <div className="flex flex-col w-full justify-center">
+                        <h2 className="text-4xl font-semibold text-zmain text-center mb-4">Somos consultoría en sostenibilidad</h2>
+                        <div className="border-t-[1px] w-full border-zmain mb-4" />
+                        <p className="text-gray-700 text-center">
+                            Convertimos el compromiso con el triple impacto en acciones concretas, diseñando e implementando estrategias que hacen de la sostenibilidad
+                            una realidad tangible para empresas y organizaciones públicas y privadas.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
