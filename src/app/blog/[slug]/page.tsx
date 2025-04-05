bb55ca814f02a48df8537e7db589b1f2793805e5
Tomás <tomasplleonart@gmail.com>
2025-04-05 17:22:56 +0200

    rework

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,16 +1,9 @@
 import { Blog } from "@/modules/blog/Blog";
-import { dummyBlogPosts } from "@/modules/shared/lib/dummy-blog-data";
 
 interface BlogPostPageProps {
   params: {
     slug: string;
   };
-}
-
-export function generateStaticParams() {
-  return dummyBlogPosts.map((post) => ({
-    slug: post.slug,
-  }));
 }
 
 export default function BlogPostPage({ params }: BlogPostPageProps) {
