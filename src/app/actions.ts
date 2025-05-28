1b72431c43be245f66a2c511ab8c0e4093b873b3
Tomás <tomasplleonart@gmail.com>
2025-05-28 10:39:51 +0200

    fix: emails

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,37 +1,39 @@
 type ActionResult = {
-    success?: boolean,
-    message?: string,
-    error?: string
-}
+  success?: boolean;
+  message?: string;
+  error?: string;
+};
 
-export async function enviarFormulario(formData: FormData): Promise<ActionResult> {
-    const name = formData.get('name') as string | null
-    const email = formData.get('email') as string | null
-    const content = formData.get('content') as string | null
+export async function enviarFormulario(
+  formData: FormData
+): Promise<ActionResult> {
+  const name = formData.get("name") as string | null;
+  const email = formData.get("email") as string | null;
+  const content = formData.get("content") as string | null;
 
-    if (!name || !email || !content) {
-        return {
-            error: 'Por favor complete todos los campos.'
-        }
+  if (!name || !email || !content) {
+    return {
+      error: "Por favor complete todos los campos.",
+    };
+  }
+
+  try {
+    const response = await fetch("http://localhost:3000/api/send-mail", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, content }),
+    });
+
+    if (!response.ok) {
+      return {
+        success: false,
+        message: "Error al enviar correo.",
+      };
     }
 
-    try {
-        const response = await fetch('http://localhost:3000/api/send-mail', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name, email, content })
-        })
-
-        if (!response.ok) {
-            return {
-                success: false,
-                message: 'Error al enviar correo.'
-            }
-        }
-
-        return { success: true, message: 'Contacto enviado con éxito!' }
-    } catch (error) {
-        console.error(error)
-        return { error: 'Error al enviar contacto.' }
-    }
+    return { success: true, message: "Contacto enviado con éxito!" };
+  } catch (error) {
+    console.error(error);
+    return { error: "Error al enviar contacto." };
+  }
 }
--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,45 +1,50 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createTransport } from 'nodemailer'
+import { createTransport } from "nodemailer";
 
 interface SendEmailRequestBody {
-    name: string;
-    email: string;
-    content: string;
+  name: string;
+  email: string;
+  content: string;
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-    try {
-        const body: SendEmailRequestBody = await req.json();
+  try {
+    const body: SendEmailRequestBody = await req.json();
 
-        const { name, email, content } = body;
+    const { name, email, content } = body;
 
-        if (!name || !email || !content) {
-            return NextResponse.json({ error: 'Faltan datos' }, { status: 400 });
-        }
+    if (!name || !email || !content) {
+      return NextResponse.json({ error: "Faltan datos" }, { status: 400 });
+    }
 
-        const transporter = createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
+    const transporter = createTransport({
+      host: "c2810738.ferozo.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-        const mailOptions = {
-            from: `"Contacto Zephyra" <${email}>`,
-            to: 'zephyracs.info@gmail.com',
-            subject: 'Nuevo contacto',
-            text: `
+    const mailOptions = {
+      from: `"Contacto Zephyra" <${email}>`,
+      to: "info@zephyraconsultora.com",
+      subject: "Nuevo contacto",
+      text: `
             RECIBISTE UN NUEVO CONTACTO DE ${name} ${email}:  
             ${content}
-        `
-        };
+        `,
+    };
 
-        await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 
-        return NextResponse.json({ message: "Envio satisfactorio" })
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: 'Error al procesar la solicitud' }, { status: 500 });
-    }
+    return NextResponse.json({ message: "Envio satisfactorio" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Error al procesar la solicitud" },
+      { status: 500 }
+    );
+  }
 }
